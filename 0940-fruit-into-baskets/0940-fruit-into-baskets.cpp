392d48088379d82e28d52a2af2af22539e3f88c3
Ratan kumar <ratankrc072@gmail.com>
2025-09-30 17:08:17 +0530

    Time: 45 ms (34.21%), Space: 79 MB (7.65%) - LeetHub

---
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int l=0;  int r=0;  int maxlen=0;  
+        std::map<int, int> map;
+        while(r<fruits.size()){
+            map[fruits[r]]++;
+            if(map.size() > 2){
+                while(map.size() > 2){
+                    map[fruits[l]]--;
+                    if(map[fruits[l]]==0){
+                        map.erase(fruits[l]);
+                    }
+                    l++;
+                }
+            }
+            if(map.size() <= 2){
+                maxlen = max(maxlen, r-l+1);
+            }
+            r++;
+        }
+        return maxlen;
+        
+    }
+};

b6295d882f4bd42ce9f027ea6d683accf56ab078
Ratan kumar <ratankrc072@gmail.com>
2025-10-05 13:09:45 +0530

    Time: 12 ms (37.33%), Space: 71.5 MB (43.14%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -4,6 +4,7 @@ public:
         return atmost(nums, k)-atmost(nums, k-1);
     }
     int atmost(vector<int>& nums, int k){
+        if (k < 0) return 0;
         int l=0; int r=0; int sum=0; int count=0;
         while(r<nums.size()){
             sum = sum+ nums[r]%2;

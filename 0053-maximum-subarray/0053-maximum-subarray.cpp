aadada38520f6698fb54515fc65ce093ce754791
Ratan kumar <ratankrc072@gmail.com>
2026-02-04 22:06:41 +0530

    Time: 0 ms (100%), Space: 71.8 MB (51.35%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -7,15 +7,15 @@ public:
         int sum = 0;
         int maxsum = -10000;
         for (int i = 0; i < nums.size(); i++) {
-            // sum = sum + nums[i];
-             
-             sum = max( nums[i], sum + nums[i]);
-            // if (sum < 0) {
-            //     sum = 0;
-            // }
-
-            maxsum = max(maxsum, sum);
+           sum += nums[i];
+            if(maxsum < sum){
+            maxsum = sum;
         }
+           if(sum < 0){
+            sum=0;
+           }
+        }
+       
         return maxsum;
     }
 };
